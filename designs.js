764941eb8645ee4e20df99a4b41e83faef7a261f
lawal bolaji <lawalabdulrasheedbolaji@gmail.com>
2018-07-01 23:46:06 +0100

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
 // Select color input
 // Select size input
 
+var color;
+var gridHeight;
+var gridWidth;
+
 // When size is submitted by the user, call makeGrid()
+$("#submitButton").click(function(event){
+	event.preventDefault();
 
-function makeGrid() {
+	gridHeight=$("#inputHeight").val();
+	gridWidth=$("#inputWeight").val();
 
-// Your code goes here!
+	makeGrid(gridHeight,gridWidth);
+});
+
+
+function makeGrid(height, width) {
+
+	for(var index=0;index<height;index++){
+		$("#pixelCanvas").append("<tr id=\"table"+height+"\"></tr>");
+
+		for(var index1=0;index1<width;index1++){
+			$("tr").filter(":last").append("<td></td>");
+		}
+	}
+
+	$("td").click(function(event){
+
+		color=$("#colorPicker").val();
+		if($(this).attr("style")){
+			$(this).removeAttr("style");
+		}
+		else{
+			$(this).css("background-color",color);
+		}
+	});
 
 }
